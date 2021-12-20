70966a0da092ee55adc38f24fc27b370fa9867fe
Budiman Rasyid <brasyid15@gmail.com>
2021-12-20 16:07:41 +0700

    Mengirim 2 data yaitu name dan message berhasil, akan tetapi menerima data masih belum bisa

--- lib/app/modules/chat/controllers/chat_controller.dart
+++ lib/app/modules/chat/controllers/chat_controller.dart
@@ -10,64 +10,59 @@ class ChatController extends GetxController {
   var chatMessage = <MessageModel>[].obs;
 
   var statusConnection = ''.obs;
-  var connectionId = ''.obs;
 
-  Future<void> startConnection() async {
-    await recreateConnection();
-    print('Connection Started');
-  }
-
-  Future recreateConnection() async {
-    print('HomeController.startConnection');
-
-    signalR = SignalR(
-      // 'http://10.0.2.2:8321/', // Emulator
-      'http://192.168.1.71:8321/', // Wifi Kantor
-      // 'http://192.168.1.104:8321/', // Wifi Rumah
-      "ChatHub",
-      hubMethods: ["addNewMessageToPag"],
-      statusChangeCallback: (status) {
-        print("statusChangeCallback: $status");
-        statusConnection.value = status.toString();
-      },
-      hubCallback: _onNewMessage,
-    );
-
-    await signalR.connect();
-  }
-
-  _onNewMessage(String? methodName, dynamic message) {
-    print('MethodName = $methodName, Message = $message');
-  }
-
-  Future<void> sendMessage(
-      {required String name, required String message}) async {
-    var messageJson = {
-      // "id": signalR.connectionId,
-      "name": name,
-      "message": message,
-      // "date": DateTime.now().toIso8601String(),
-    };
-
-    final res = await signalR.invokeMethod('Send', arguments: [
-      name,
-      message,
-    ]).catchError((error) {
-      print("Error invokeMethod: ${error.toString()}");
-    });
-
-    chatMessage.add(MessageModel.fromJson(messageJson));
-
-    print("response: $res");
-
-    chatController.clear();
-  }
+  final serviceUrl = 'http://192.168.1.71:8321/';
 
   @override
   void onInit() {
     super.onInit();
     chatController = TextEditingController();
     startConnection();
+  }
+
+  Future<void> startConnection() async {
+    await recreateConnection();
+    print('Connection Started');
+  }
+
+  Future<void> recreateConnection() async {
+    print('HomeController.startConnection');
+    signalR = SignalR(
+      serviceUrl,
+      "ChatHub",
+      hubMethods: ['addNewMessageToPage'],
+      statusChangeCallback: (status) {
+        print("statusChangeCallback: $status");
+        statusConnection.value = status.toString();
+      },
+      hubCallback: _onNewMessage,
+    );
+    await signalR.connect();
+  }
+
+  _onNewMessage(String methodName, dynamic message) {
+    print('onNewMessage');
+    print('MethodName = $methodName, Message = $message');
+  }
+
+  Future<void> sendMessage(
+      {required String name, required String message}) async {
+    var messageJson = {
+      "name": name,
+      "message": message,
+    };
+    final res = await signalR.invokeMethod('Send', arguments: [
+      name,
+      message,
+    ]).catchError((error) {
+      print("Error invokeMethod: ${error.toString()}");
+    }).then((value) {
+      print("value: $value");
+    });
+    print("response: $res");
+    chatMessage.add(MessageModel.fromJson(messageJson));
+    chatController.clear();
+    update();
   }
 
   @override
--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:latihan_signalr_framework_chat/constant.dart';
 
 import '../controllers/chat_controller.dart';
